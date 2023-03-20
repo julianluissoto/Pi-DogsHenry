d57b7d995267e2c3c8dc7002c2e166754a37243c
julianluisasoto <julianlasoto@hotmail.com>
2023-03-20 20:10:25 -0300

    new database on railway

--- api/index.js
+++ api/index.js
@@ -1,25 +1,6 @@
-//                       _oo0oo_
-//                      o8888888o
-//                      88" . "88
-//                      (| -_- |)
-//                      0\  =  /0
-//                    ___/`---'\___
-//                  .' \\|     |// '.
-//                 / \\|||  :  |||// \
-//                / _||||| -:- |||||- \
-//               |   | \\\  -  /// |   |
-//               | \_|  ''\---/''  |_/ |
-//               \  .-\__  '-'  ___/-. /
-//             ___'. .'  /--.--\  `. .'___
-//          ."" '<  `.___\_<|>_/___.' >' "".
-//         | | :  `- \`.;`\ _ /`;.`/ - ` : | |
-//         \  \ `_.   \_ __\ /__ _/   .-` /  /
-//     =====`-.____`.___ \_____/___.-`___.-'=====
-//                       `=---='
-//     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
-
+require("dotenv").config();
 const axios = require("axios");
 const { Dog, Temperament } = require("./src/db");
 
@@ -54,9 +35,6 @@ async function precharge() {
 }
 
 // Syncing all the models at once.
-conn.sync({ force: false }).then( () => {
-  
-  server.listen(process.env.PORT, () => {
-    console.log("server listening at 3001"); // eslint-disable-line no-console
-  });
+conn.sync({ force: false }).then(() => {
+  server.listen(process.env.PORT, () => {});
 });
