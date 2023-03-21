3f80588968c4c33db98bc80fcd722c4184f3e477
julianluisasoto <julianlasoto@hotmail.com>
2023-03-21 13:18:05 -0300

    test db on local host

--- client/src/index.js
+++ client/src/index.js
@@ -9,8 +9,8 @@ import { BrowserRouter } from "react-router-dom";
 import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
-axios.defaults.baseURL =
-  /* process.env.REACT_APP_API || */ "http://localhost:3001";
+axios.defaults.baseURL = "http://localhost:6217";
+/* process.env.REACT_APP_API || */
 
 ReactDOM.render(
   <React.StrictMode>
