d827827050156420c26b4c2b0db755a0dcb63bb5
julianluisasoto <julianlasoto@hotmail.com>
2023-08-11 15:17:50 -0300

    add new api axios default

--- client/src/index.js
+++ client/src/index.js
@@ -10,7 +10,7 @@ import axios from "axios";
 import dotenv from "dotenv";
 dotenv.config();
 axios.defaults.baseURL =
-  "pi-dogs-henry-api.vercel.app" || "http://localhost:5432";
+  "https://pi-dogs-henry-api.vercel.app/" || "http://localhost:5432";
 
 ReactDOM.render(
   <React.StrictMode>
